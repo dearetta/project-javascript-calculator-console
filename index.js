6acd4ff4de301cf020c9985983e2784152b98ce7
dearettaa@gmail.com <dearettaa@gmail.com>
2019-06-25 11:32:33 +0700

    calculator console

---
+++ index.js
@@ -0,0 +1,17 @@
+let firstNum = prompt('Enter First Number')
+let secondNum = prompt('Enter Second Number')
+
+const resultAddition = firstNum + secondNum
+const resultSubstraction = firstNum - secondNum
+const resultMultiplication = firstNum * secondNum
+const resultDivison = firstNum / secondNum
+
+console.log(`Your calculating for ${firstNum} and ${secondNum}`)
+
+console.log(`Addition: ${resultAddition}`)
+console.log(`Substraction: ${resultSubstraction}`)
+console.log(`Multiplication: ${resultMultiplication}`)
+console.log(`Division: ${resultDivison}`)
+
+
+
